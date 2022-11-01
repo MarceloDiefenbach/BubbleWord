22325fe02987bd0f8be1c8c8ea717f3835ed5123
Marcelo Diefenbach <58398373+MarceloDiefenbach@users.noreply.github.com>
2022-11-01 17:24:17 -0300

    Initial Commit

---
+++ BubbleWord/BubbleWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleWordApp.swift
+//  BubbleWord
+//
+//  Created by Marcelo Diefenbach on 01/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BubbleWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
