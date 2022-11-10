9ec4dc3ee54997248235f81ad2e60edfc39cc031
Marcelo Diefenbach <58398373+MarceloDiefenbach@users.noreply.github.com>
2022-11-10 14:35:15 -0300

    logic of playing game
    - generate new set of letters
    - verify if all letter already be used
    - flip card when all letters already be used

--- BubbleWord/Game/GameViewModel.swift
+++ BubbleWord/Game/GameViewModel.swift
@@ -29,6 +29,7 @@ class GameViewModel: ObservableObject {
         Letter(letter: "L", state: .active, colorIndex: 1)
     ]
     @Published var flipped: Bool = false
+    private var controlIfGameFinish: Int = 0
     
     //MARK: - functions
     
@@ -36,8 +37,73 @@ class GameViewModel: ObservableObject {
         if letters[index].state == .active {
             letters[index].state = .inactive
             timeRemaining = 30
+            controlIfGameFinish += 1
         }
         
+        if controlIfGameFinish == 12 {
+            controlIfGameFinish = 0
+            // change card and generate new set of letters
+            generateNewSetOfLetters()
+            changeCard()
+            
+        }
+    }
+    
+    func changeCard() {
+        //logic to change card label and color
+        flipped.toggle()
+    }
+    
+    func generateNewSetOfLetters() {
+        
+        let randomInt = Int.random(in: 1 ... 3)
+        
+        if randomInt == 1 {
+            letters = [
+                Letter(letter: "T", state: .active, colorIndex: 1),
+                Letter(letter: "P", state: .active, colorIndex: 2),
+                Letter(letter: "A", state: .active, colorIndex: 3),
+                Letter(letter: "F", state: .active, colorIndex: 1),
+                Letter(letter: "R", state: .active, colorIndex: 2),
+                Letter(letter: "Z", state: .active, colorIndex: 3),
+                Letter(letter: "F", state: .active, colorIndex: 1),
+                Letter(letter: "J", state: .active, colorIndex: 2),
+                Letter(letter: "L", state: .active, colorIndex: 1),
+                Letter(letter: "Q", state: .active, colorIndex: 2),
+                Letter(letter: "W", state: .active, colorIndex: 3),
+                Letter(letter: "O", state: .active, colorIndex: 1)
+            ]
+        } else if randomInt == 2 {
+            letters = [
+                Letter(letter: "D", state: .active, colorIndex: 1),
+                Letter(letter: "I", state: .active, colorIndex: 2),
+                Letter(letter: "G", state: .active, colorIndex: 3),
+                Letter(letter: "T", state: .active, colorIndex: 1),
+                Letter(letter: "Q", state: .active, colorIndex: 2),
+                Letter(letter: "L", state: .active, colorIndex: 3),
+                Letter(letter: "K", state: .active, colorIndex: 1),
+                Letter(letter: "X", state: .active, colorIndex: 2),
+                Letter(letter: "U", state: .active, colorIndex: 1),
+                Letter(letter: "J", state: .active, colorIndex: 2),
+                Letter(letter: "A", state: .active, colorIndex: 3),
+                Letter(letter: "O", state: .active, colorIndex: 1)
+            ]
+        } else if randomInt == 3 {
+            letters = [
+                Letter(letter: "A", state: .active, colorIndex: 1),
+                Letter(letter: "M", state: .active, colorIndex: 2),
+                Letter(letter: "D", state: .active, colorIndex: 3),
+                Letter(letter: "R", state: .active, colorIndex: 1),
+                Letter(letter: "K", state: .active, colorIndex: 2),
+                Letter(letter: "W", state: .active, colorIndex: 3),
+                Letter(letter: "P", state: .active, colorIndex: 1),
+                Letter(letter: "O", state: .active, colorIndex: 2),
+                Letter(letter: "I", state: .active, colorIndex: 1),
+                Letter(letter: "V", state: .active, colorIndex: 2),
+                Letter(letter: "S", state: .active, colorIndex: 3),
+                Letter(letter: "M", state: .active, colorIndex: 1)
+            ]
+        }
     }
     
     //MARK: - time control
