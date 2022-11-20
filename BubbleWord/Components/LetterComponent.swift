623ebd80be567f89dd219674709fe13ca577a8c5
Marcelo Diefenbach <58398373+MarceloDiefenbach@users.noreply.github.com>
2022-11-20 10:27:56 -0300

    apply tokens on letter component

--- BubbleWord/Components/LetterComponent.swift
+++ BubbleWord/Components/LetterComponent.swift
@@ -33,14 +33,14 @@ struct LetterComponent: View {
                 }
                 Text(letter.letter)
                     .foregroundColor(.white)
-                    .font(.system(size: 30, weight: .bold, design: .default))
+                    .font(.system(size: FontSize.large.value, weight: .bold, design: .default))
             } else {
                 Image("Inactive")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                 Text(letter.letter)
                     .foregroundColor(.white)
-                    .font(.system(size: 30, weight: .bold, design: .default))
+                    .font(.system(size: FontSize.large.value, weight: .bold, design: .default))
                     .opacity(0.5)
             }
         }.cornerRadius(CornerRadius.circular.value)
